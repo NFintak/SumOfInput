dfc69e03c1cba10f1555a33a9cbd135217fd46a3
Nathan <natefintak@gmail.com>
2025-02-28 13:23:06 -0500

    in progress

--- src/main/java/SumOfInput.java
+++ src/main/java/SumOfInput.java
@@ -1,1 +1,29 @@
-//you should be able to see this comment
+import java.util.Scanner;
+
+public class SumOfInput {
+    public class void main(String[] args) {
+        int num = userInput();
+        int sum = sumNumbers();
+        System.out.println(sum);
+    }
+
+    boolean testSumNumbers() {
+        return sumNumbers(5) == 15;
+    }
+
+    int userInput() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pick a number: ");
+        int number = scan.nextInt();
+        scan.close();
+        return number;
+    }
+
+    int sumNumbers(num) {
+        total = 0;
+        for (int i = 0; i <= num; i++) {
+            total = total + i;
+        }
+        return total;
+    }
+}
