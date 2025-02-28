a42b5e105cad0abda496422b4825833c4c0aeed7
Nathan <natefintak@gmail.com>
2025-02-28 18:19:22 -0500

    Add  extra credit

--- src/main/java/SumOfInput.java
+++ src/main/java/SumOfInput.java
@@ -9,22 +9,32 @@ public class SumOfInput {
 
         int num = Integer.parseInt(scan.nextLine()); //assigns input from prompt to num
 
-        System.out.println(sumNumbers(num)); //prints sum of numbers 0 thru num
+        System.out.println(sumNumbers(num));
 
-        System.out.println(tbd(num)); //prints sum of numbers 0 thru num
+        System.out.println(gaussianCalc(num));
     }
 
     public static int sumNumbers(int n) {
+        long startTimeSum, endTimeSum, elapsedTimeSum;
         int total = 0;
+        startTimeSum = System.currentTimeMillis();
         for (int i = 0; i <= n; i++) {
             total = total + i;
         }
+        endTimeSum = System.currentTimeMillis();
+        elapsedTimeSum = endTimeSum - startTimeSum;
+        System.out.println("sumNumbers elapsed time: " + elapsedTimeSum); //output was 0
         return total;
     }
 
-    public static int tbd(int x) {
-        return (x * (x+1))/2;
+    public static int gaussianCalc(int x) {
+        long startTimeGauss, endTimeGauss, elapsedTimeGauss;
+        startTimeGauss = System.currentTimeMillis();
+        x = (x * (x+1))/2;
+        endTimeGauss = System.currentTimeMillis();
+        elapsedTimeGauss = endTimeGauss - startTimeGauss; //output was 0
+        System.out.println("gaussianCalc elapsed time: " + elapsedTimeGauss);
+        return x;
     }
-
 }
 
